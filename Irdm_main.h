7ed42825c6761d8276bc10d7b923816033d8ccbd
FrankZhang2013 <921809772@qq.com>
2014-02-28 14:43:43 +0800

    Create Irdm_main.h

---
+++ Irdm_main.h
@@ -0,0 +1,17 @@
+#ifndef	_IRDM_MAIN_H
+#define	_IRDM_MAIN_H
+
+/////////////////////// Add header files needed below //////////////////////////////////
+#include "Irdm_Interface.h"
+#include "njkk_interface.h"
+
+#include "OpenPort_Modem_state_check.h"
+#include "SBD_Check.h"
+#include "SBD_FTP_Classify.h"
+#include "SBD_Upload.h"
+#include "FTP_Upload.h"
+#include "FTP_Download.h"
+#include "SJ_heart.h"
+
+
+#endif
