bc891b56f6f5cd886ea36e1efd486725c92dbbf7
FrankZhang2013 <921809772@qq.com>
2014-02-28 14:41:06 +0800

    Create FTP_Download.h

---
+++ FTP_Download.h
@@ -0,0 +1,19 @@
+#ifndef	_FTP_DOWNLOADFTP_H
+#define	_FTP_DOWNLOADFTP_H
+
+/////////////////////// Add header files needed below //////////////////////////////////
+#include "njkk_interface.h"
+#include "Irdm_Interface.h"
+#include "Irdm_main.h"
+//////////////////// Add new macro variables definition below ////////////////////////////////
+
+//////////////////// Add new global variables definition below //////////////////////////////
+
+
+//////////////////// Add new struct variables definition below //////////////////////////////
+
+
+//////////////////// Add new function declare below ////////////////////////////////////////
+int FTP_Downloadftp(int pc104_state,static_config *jtpz,dynamic_config *dtpz);
+
+#endif
