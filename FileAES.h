23b2aaf4c51e94a1122cd9a50e844f35d11c91fa
FrankZhang2013 <921809772@qq.com>
2014-03-25 12:16:40 +0800

    Create FileAES.h

---
+++ FileAES.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <string.h>
+#include "aes.h"
+
+int encry_file(char* file,char* encry_file_name);
+int decry_file(char* file,char* decry_file_name);
